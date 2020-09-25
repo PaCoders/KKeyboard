c7fe0cb6fc0965bdef32df27c0292fb41b43a877
PaCoders <paco.chanivetsanchez@alum.uca.es>
2020-09-25 16:44:59 +0200

    Cambios en el constructor "Keyboard"

--- bin/keyboard/notes.cpp
+++ bin/keyboard/notes.cpp
@@ -1,14 +1,29 @@
 #include "notes.h"
-#include <fstream>
+#include <stdio.h>
+#include <string.h>
 
 Keyboard::Keyboard(int l): tones(new double[l]){
-    ifstream f("frecuencias.txt");
-    int i = 0;
-    while(!f.eof()){
-        f>>tones[i];
-        i++;
+    FILE *f;
+    f = fopen("frecuencias.txt","r");
+
+    if(f == NULL){
+        exit(1);
+        fclose(f);
     }
-    f.close();
+    
+    else{
+        int i = 0;
+        string fr;
+
+        while(!f.eof()){
+            f>>fr;
+            tones[i] = atof(fr);
+            i++;
+        }
+
+        fclose(f);
+    }
+    
 }
 
 const double Keyboard::tocar(int key) const{return tones[key];}
