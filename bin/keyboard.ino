7bca464071955f5df7d28ce13891bb2ef5c64041
PaCoders <paco.chanivetsanchez@alum.uca.es>
2020-09-25 11:21:05 +0200

    Primera actualizacion

---
+++ bin/keyboard.ino
@@ -0,0 +1,17 @@
+
+//Piano en arduino
+
+const int pRed = 3;
+const int pBlue = 5;
+const int pGreen = 6;
+const int buzz = 2;
+
+void setup() {
+  pinMode(pRed, OUTPUT); //Conectamos las conexiones del LED RGB
+  pinMode(pBlue, OUTPUT);
+  pinMode(pGreen, OUTPUT);
+}
+
+void loop() {
+  
+}
