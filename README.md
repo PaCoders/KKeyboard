fbe8c9c530f4bdff05c92c8ad4ea2b4fef70d30f
PaCoders <paco.chanivetsanchez@alum.uca.es>
2020-09-25 12:19:59 +0200

    Cambios varios

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# KKeyboard
+
+# ¿Quieres tocar el piano pero no tienes?
+_¡Pues ha llegado tu oportunidad si tienes una placa Arduino UNO R3! Si quieres tocar el piano es tan facil como descargar este código_
+
+# ¿Qué componentes necesito?
+_Para poder utilizar el piano necesitarás los siguientes componentes:_
+* Buzzer
+* LED RGB
+* 3 resistencias 330 Ohms
+* Cables (está claro)
+
+# ¿Cómo conecto cada componente?
+_Te dejo por aquí abajo un esquema para que puedas ver cómo se puede conectar:_
+![alt text](https://github.com/PaCoders/KKeyboard)
--- bin/keyboard/keyboard.ino
+++ bin/keyboard/keyboard.ino
@@ -1,21 +1,30 @@
 
 //Piano en arduino
 
-const int pRed = 3;
+//--------- CONEXIONES UTILIZADAS --------------
+const int pRed = 3; //LED RGB
 const int pBlue = 5;
 const int pGreen = 6;
-const int buzz = 2;
+const int buzz = 2; //Buzzer
 
 void setup() {
   pinMode(pRed, OUTPUT); //Conectamos las conexiones del LED RGB
   pinMode(pBlue, OUTPUT);
   pinMode(pGreen, OUTPUT);
+  inicio();
   pinMode(buzz, OUTPUT); //Conectamos el buzzer
 }
 
 void loop() {
+  
+}
+
+void inicio(){ //El LED conectado nos indica que esta todo OK en el sistema
   analogWrite(pRed, 19);
   analogWrite(pBlue, 222);
   analogWrite(pGreen, 240);
+  delay(1000);
+  digitalWrite(pRed,LOW);
+  digitalWrite(pBlue,LOW);
+  digitalWrite(pGreen,LOW);
 }
-
